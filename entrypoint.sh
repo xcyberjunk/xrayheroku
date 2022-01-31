455250958bec3e193260613b5112fb3dbf03ce94
xcyberjunk <60675120+xcyberjunk@users.noreply.github.com>
2022-01-31 19:49:00 +0700

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#Config xray
+
+rm -rf /etc/xray/config.json
+cat << EOF > /etc/xray/config.json
+{
+  "inbounds": [
+    {
+     
+      "port": $PORT,
+      "protocol": "vmess",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+#run xray
+
+xray -c /etc/xray/config.json
